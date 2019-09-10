8c16a91a297a0823a4790cdcdaef1433123e7aaf
kudog <kudo.gergely@kulcs-soft.hu>
2019-09-10 08:47:30 +0200

    annotation changed

--- src/main/java/com/ks/zuul/KulcsSoftZuulServer.java
+++ src/main/java/com/ks/zuul/KulcsSoftZuulServer.java
@@ -2,10 +2,10 @@ package com.ks.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringBootApplication
-@EnableZuulProxy
+@EnableZuulServer
 public class KulcsSoftZuulServer {
 
     public static void main(String[] args) {
