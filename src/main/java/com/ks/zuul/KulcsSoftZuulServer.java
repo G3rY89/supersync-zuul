52df0412fba3f553006700974031bb15b929381f
kudog <kudo.gergely@kulcs-soft.hu>
2019-09-10 10:16:40 +0200

    dfg

--- src/main/java/com/ks/zuul/KulcsSoftZuulServer.java
+++ src/main/java/com/ks/zuul/KulcsSoftZuulServer.java
@@ -2,9 +2,11 @@ package com.ks.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.*;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableZuulServer
 public class KulcsSoftZuulServer {
 
