8b0e6db4ffb14462b2f641a48fe0db193ccd84d0
kudog <kudo.gergely@kulcs-soft.hu>
2019-09-10 10:21:09 +0200

    sdfgsdf

--- src/main/java/com/ks/zuul/KulcsSoftZuulServer.java
+++ src/main/java/com/ks/zuul/KulcsSoftZuulServer.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.zuul.*;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableZuulServer
+@EnableZuulProxy
 public class KulcsSoftZuulServer {
 
     public static void main(String[] args) {
