68d5e5e7d08df3af15c89fbeafe45c922a5747e1
kudog <kudo.gergely@kulcs-soft.hu>
2019-09-13 11:29:09 +0200

    spring version update

--- src/main/java/com/ks/zuul/KulcsSoftZuulServer.java
+++ src/main/java/com/ks/zuul/KulcsSoftZuulServer.java
@@ -3,11 +3,13 @@ package com.ks.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.*;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
+@EnableHystrix
 public class KulcsSoftZuulServer {
 
     public static void main(String[] args) {
